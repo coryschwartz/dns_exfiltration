b3a3b47beb39def6d4755ad71f1051f567d37ee9
Cory <cory.is.evil@gmail.com>
2017-07-09 13:47:07 -0500

    syntax error

--- fake-dns-server/dns_exfil/cli/server.py
+++ fake-dns-server/dns_exfil/cli/server.py
@@ -5,7 +5,7 @@ import os
 @click.option('--domain', default='def.con', help='spoof Domain name to use in server responses')
 @click.option('--ip', default='192.168.1.1', help='spoof IP address to use in server responses')
 @click.option('--cmd', default='cmd', help='File for sending commands through MX records')
-@click.option('--basedir' default=os.getcwd())
+@click.option('--basedir', default=os.getcwd())
 def bot(domain, ip, cmd, basedir):
     print('DNS exfil Server Bot')
     print(locals())
