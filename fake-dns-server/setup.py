efa3b1a57de634a001039517c5a8c5a8b5dc4f1d
Cory <cory.is.evil@gmail.com>
2017-07-09 11:45:13 -0500

    added other files

---
+++ fake-dns-server/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+with open('LICENSE.txt') as f:
+    license = f.read()
+
+with open('README.txt') as f:
+    long_description = f.read()
+
+setup(
+    description='A DNS Exfiltration server',
+    entry_points = {
+        'console_scripts': [
+            'exfil_serve=dns_exfil.cli.server:main',
+            'exfil_upload=dns_exfil.cli.client:upload',
+            'exfil_download=dns_exfil.cli.client:download'
+        ]
+    },
+    license=license,
+    long_description=long_description,
+    name='dns_exfil',
+    packages=['dns_exfil'],
+    version='3',
+
+)
