5de3465f7c78db4d880de47ed88adf8ec77d7be4
Cory <cory.is.evil@gmail.com>
2017-07-23 03:50:02 -0500

    adds chunk client and provided cli

---
+++ fake-dns-server/dns_exfil/exfiltrators/base/client.py
@@ -0,0 +1,4 @@
+# I think this class will self-register the cli.
+
+class BaseClient:
+    pass
---
+++ fake-dns-server/dns_exfil/exfiltrators/chunkdownloader/cli.py
@@ -0,0 +1,37 @@
+from dns_exfil.exfiltrators.chunkdownloader.client import ChunkClient
+
+@click.command(name='download')
+@click.option('--chunk_size', help='size in bytes to transfer in each packet', default=30, type=int)
+@click.option('--domain', default='example.com', help='ending domain name')
+@click.argument('connect')
+@click.argument('filename')
+def chunk_download(chunk_size, domain, connect, filename):
+    client = ChunkClient(domain, connect)
+    client.download(filename, chunk_size)
+
+@click.command(name='list')
+@click.option('--domain', default='example.com', help='ending domain name')
+@click.argument('connect')
+def chunk_list(domain, connect):
+    client = ChunkClient(domain, connect)
+    index = client.get_index()
+    for element in index:
+        print(element['name'])
+
+@click.command(name='upload')
+@click.option('--chunk_size', help='size in bytes to transfer in each packet', default=30, type=int)
+@click.option('--domain', default='example.com', help='ending domain name')
+@click.argument('connect')
+@click.argument('filename')
+def chunk_upload(chunk_size, domain, connect, filename)
+    client = ChunkClient(domain, connect)
+    client.upload(filename, chunk_size)
+
+@click.group()
+def chunk():
+    pass
+
+
+chunk.add_command(chunk_download)
+chunk.add_command(chunk_list)
+chunk.add_command(chunk_upload)
